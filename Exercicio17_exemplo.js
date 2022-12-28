532a510b26102d3853e921b5814b7e7282cf7804
Daniel Bastianelle <120055219+Ryomabastian@users.noreply.github.com>
2022-12-28 21:43:50 +0000

    Add files via upload

---
+++ Exercicio17_exemplo.js
@@ -0,0 +1,23 @@
+var prompt = require('prompt-sync')();
+
+let numero = prompt("Caga: " )
+
+
+
+// if (numero<0){
+
+//     console.log(numero);
+
+// } else {
+
+//     let coco = 1/numero
+
+//     console.log(coco);
+
+// }
+
+// ou
+
+let coco = 1/numero
+
+console.log(numero<0 ? numero : coco);
