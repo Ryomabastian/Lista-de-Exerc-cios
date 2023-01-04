c716fb4c49403de57309b49efd5bbcae617078df
Daniel Bastianelle <120055219+Ryomabastian@users.noreply.github.com>
2023-01-04 14:54:31 +0000

    Add files via upload

---
+++ Exercicio32-Switch-Case.js
@@ -0,0 +1,48 @@
+//Elabore um algoritmo, segundo a notação gráfica, que aceite um número inteiro entre 1 e 12 para apresentar o mês correspondente. 
+//Denunciar qualquer valor introduzido como inválido.
+
+//VIDEO SWITCH CASE: https://www.youtube.com/watch?v=-11qkJczuqo
+
+var prompt = require('prompt-sync')();
+var numero = parseInt(prompt('Numero: '));
+
+switch(numero){
+    case 1:
+        console.log('janeiro')
+        break;
+    case 2:
+        console.log('fevereiro')
+        break;
+    case 3:
+        console.log('março')
+        break;
+    case 4:
+        console.log('abril')
+        break;
+    case 5:
+        console.log('maio')
+        break;
+    case 6:
+        console.log('junho')
+        break;
+    case 7:
+        console.log('julho')
+        break;
+    case 8:
+        console.log('agosto')
+        break;
+    case 9:
+        console.log('setembro')
+        break;
+    case 10:
+        console.log('outubro')
+        break;
+    case 11:
+        console.log('novembro')
+        break;
+    case 12:
+        console.log('dezembro')
+        break;
+
+    default: console.log('inválido')
+}
