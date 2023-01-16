536009741f8d1fe27b9fc81ff53edbaac376c32f
Daniel Bastianelle <120055219+Ryomabastian@users.noreply.github.com>
2023-01-16 19:51:35 +0000

    Add files via upload

---
+++ Exercicio48-Criar-Array-Autom-IMPAR.js
@@ -0,0 +1,27 @@
+var array = []
+let total = []
+
+for (let j = 0; j <= 500; j++) {
+
+    if (j%2 != 0) {
+        var acumulador = j
+        array.push(acumulador)
+    }
+
+} console.log(array)
+
+
+////////// OU
+
+for (let i = 1; i < 601; i++) {
+    if (i%2 == 0) {
+        array.push(i)
+    }
+} console.log(array)
+
+
+////////// OU
+
+
+
+
